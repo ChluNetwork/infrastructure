938f840edd8380c53e69be0a305c179b6fb6bd47
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-08-01 12:36:49 +0200

    add new test stubs

---
+++ tests/api.test.js
@@ -0,0 +1,20 @@
+
+
+describe('Integration: API Client with Query and Publish API Servers and Collector', () => {
+
+    describe('API Client with Publish API Server', () => {
+        it('stores a review record')
+        it('publishes a DID')
+    })
+
+    describe('API Client with Query API Server', () => {
+        it('reads a DID')
+        it('reads a review records')
+        it('reads review records by author')
+        it('reads review records by subject')
+    })
+
+    describe('API Client and Marketplace', () => {
+        it('registers as Vendor')
+    })
+})
