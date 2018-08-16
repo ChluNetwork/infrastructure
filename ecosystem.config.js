c23bf70fb6467e051f55297c49606afb16033541
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-08-16 17:16:16 +0200

    add configurable DB host and port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,12 +12,15 @@ env(path.join(__dirname, '.env'))
 const blockcypherToken = process.env.BLOCKCYPHER_TOKEN
 const network = process.env.CHLU_NETWORK || 'experimental'
 const marketplaceLocation = process.env.CHLU_MARKETPLACE_LOCATION || 'http://localhost:5000'
+const dbHost = process.env.CHLU_POSTGRESQL_HOST || 'localhost'
+const dbPort = process.env.CHLU_POSTGRESQL_PORT || 5432
 const dbName = process.env.CHLU_POSTGRESQL_DB
 const dbUser = process.env.CHLU_POSTGRESQL_USER
 const dbPassword = process.env.CHLU_POSTGRESQL_PASSWORD
-const databaseConfCollector = `--postgres --database-db ${dbName} --database-user ${dbUser} ${dbPassword ? `--database-password ${dbPassword}` : ''}`
-const databaseConfAPIServers = `--postgres --no-write --database-db ${dbName} --database-user ${dbUser} ${dbPassword ? `--database-password ${dbPassword}` : ''}`
-const databaseConfMarketplace = `--chlu-postgres --chlu-no-write --chlu-database-name ${dbName} --chlu-database-user ${dbUser} ${dbPassword ? `--database-password ${dbPassword}` : ''}`
+const databaseConfCollector = `--postgres --database-host ${dbHost} --database-port ${dbPort} --database-db ${dbName} --database-user ${dbUser} ${dbPassword ? `--database-password ${dbPassword}` : ''}`
+const databaseConfAPIServers = `--no-write ${databaseConfCollector}`
+// TODO: Marketplace should support --chlu-database-port
+const databaseConfMarketplace = `--chlu-postgres --chlu-no-write --chlu-database-host ${dbHost} --chlu-database-name ${dbName} --chlu-database-user ${dbUser} ${dbPassword ? `--database-password ${dbPassword}` : ''}`
 
 module.exports = {
     /**
