53f1645402b9997b34526114c47f90854267ac70
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-08-17 10:53:03 +0200

    fix marketplace chludb password option

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,7 +19,7 @@ const dbUser = process.env.CHLU_POSTGRESQL_USER
 const dbPassword = process.env.CHLU_POSTGRESQL_PASSWORD
 const databaseConfCollector = `--postgres --database-host ${dbHost} --database-port ${dbPort} --database-db ${dbName} --database-user ${dbUser} ${dbPassword ? `--database-password ${dbPassword}` : ''}`
 const databaseConfAPIServers = `--no-write ${databaseConfCollector}`
-const databaseConfMarketplace = `--chlu-postgres --chlu-no-write --chlu-database-host ${dbHost} --chlu-database-port ${dbPort} --chlu-database-name ${dbName} --chlu-database-user ${dbUser} ${dbPassword ? `--database-password ${dbPassword}` : ''}`
+const databaseConfMarketplace = `--chlu-postgres --chlu-no-write --chlu-database-host ${dbHost} --chlu-database-port ${dbPort} --chlu-database-name ${dbName} --chlu-database-user ${dbUser} ${dbPassword ? `--chlu-database-password ${dbPassword}` : ''}`
 
 module.exports = {
     /**
